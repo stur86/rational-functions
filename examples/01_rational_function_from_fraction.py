9d1b24a56e7b3f746e9b0cdd0a805f3a49177065
Simone Sturniolo <simonesturniolo@gmail.com>
2025-04-05 14:44:17 +0100

    Added exports and an example of numerical precision benefits

--- examples/01_rational_function_from_fraction.py
+++ examples/01_rational_function_from_fraction.py
@@ -1,7 +1,7 @@
 # %%
 import numpy as np
 from numpy.polynomial import Polynomial
-from rational_functions import RationalFunction
+from rational_functions import RationalFunction, RationalTerm, PolynomialRoot
 import matplotlib.pyplot as plt
 
 # %%
@@ -24,4 +24,46 @@ ax.set_title("Rational function from fraction of polynomials")
 ax.set_xlabel("x")
 ax.set_ylabel("y")
 ax.legend()
+# %%
+
+# Example of loss of numerical precision in the polynomial division
+# form
+
+deg = 70
+rng = np.random.default_rng(0)
+
+coefs_hi = np.random.normal(size=deg)
+# Poles span multiple orders of magnitude
+roots_hi = 10 ** (3 * rng.random(deg))
+
+ratfunc_hi = RationalFunction(
+    [RationalTerm(PolynomialRoot(r), c) for (c, r) in zip(coefs_hi, roots_hi)]
+)
+
+numerator_hi = ratfunc_hi.numerator
+denominator_hi = ratfunc_hi.denominator
+# %%
+
+fig, ax = plt.subplots()
+x_long = np.logspace(0, 1, 400)
+
+y_hi = numerator_hi(x_long) / denominator_hi(x_long)
+y_r_hi = ratfunc_hi(x_long)
+
+l1 = ax.plot(x_long, y_hi, label="np.Polynomial/np.Polynomial", c="k", lw=0.5)
+l2 = ax.plot(x_long, y_r_hi, label="RationalFunction", c="b", lw=1, ls="--")
+ax.set_title(f"Rational function from fraction of polynomials (deg={deg})")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_xscale("log")
+ax.set_ylim(-100, 100)
+ax.set_xlim(x_long[0], x_long[-1])
+
+# Plot the poles of the rational function
+for root in roots_hi:
+    if root > x_long[-1]:
+        continue
+    last_pl = ax.axvline(root, color="r", lw=1, label="Poles")
+
+ax.legend(handles=[*l1, *l2, last_pl])
 # %%
--- src/rational_functions/__init__.py
+++ src/rational_functions/__init__.py
@@ -1,5 +1,8 @@
 """Rational Functions Module"""
 
 from .ratfunc import RationalFunction
+from .terms import RationalTerm
+from .roots import PolynomialRoot
 
-__all__ = ["RationalFunction"]
+__all__ = ["RationalFunction", "RationalTerm", 
+           "PolynomialRoot"]
