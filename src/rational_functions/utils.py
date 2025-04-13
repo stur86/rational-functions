ba68d0d16cfe68f03f296c3c49fb07eaee63025b
Simone Sturniolo <simonesturniolo@gmail.com>
2025-04-13 11:42:52 +0100

    Added group_by_closeness function

--- src/rational_functions/utils.py
+++ src/rational_functions/utils.py
@@ -1,6 +1,7 @@
 """Utility functions for the package."""
 
 import numpy as np
+from typing import Iterable, Callable, TypeVar
 from numpy.typing import ArrayLike
 
 PolynomialDef = np.polynomial.Polynomial | ArrayLike
@@ -19,3 +20,47 @@ def as_polynomial(p: PolynomialDef) -> np.polynomial.Polynomial:
     if isinstance(p, np.polynomial.Polynomial):
         return p
     return np.polynomial.Polynomial(p)
+
+
+T = TypeVar("T")
+
+
+def group_by_closeness(
+    data: Iterable[T],
+    key: Callable[[T], complex] = lambda x: x,
+    atol: float = 1e-8,
+    rtol: float = 1e-5,
+) -> dict[complex, list[T]]:
+    """Group elements by closeness of their keys. Keys must be numeric (float or complex).
+
+    Args:
+        data: Iterable of elements to group.
+        key: Function to extract the key from each element.
+        atol: Absolute tolerance for closeness.
+        rtol: Relative tolerance for closeness.
+
+    Returns:
+        dict: Dictionary with keys as unique values and values as lists of elements.
+    """
+
+    # Start by extracting keys from the data
+    key_arr = np.array([key(d) for d in data])
+    sort_idx = np.argsort(key_arr, stable=True)
+    key_sorted = key_arr[sort_idx]
+
+    # Cluster by similarity
+    edges = (
+        np.where(~np.isclose(key_sorted[:-1], key_sorted[1:], atol=atol, rtol=rtol))[0]
+        + 1
+    )
+    # Groups
+    groups = np.split(sort_idx, edges)
+
+    # Create a dictionary to hold the grouped elements
+    grouped_dict: dict[complex, list[T]] = {}
+    for group in groups:
+        # Use the mean of the group as the key
+        key_value = np.mean(key_arr[group])
+        grouped_dict[key_value] = list([data[i] for i in group])
+
+    return grouped_dict
--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -1,5 +1,6 @@
+import numpy as np
 from numpy.polynomial import Polynomial
-from rational_functions.utils import as_polynomial
+from rational_functions.utils import as_polynomial, group_by_closeness
 
 
 def test_as_polynomial():
@@ -14,3 +15,47 @@ def test_as_polynomial():
 
     assert p_p == p
     assert (c_p.coef == c).all()
+
+
+def test_group_by_closeness():
+    # Test a simple group
+    data = [1, 3.00000002, 3, 2, 1.00000001]
+    grouped = group_by_closeness(data)
+    assert len(grouped) == 3
+
+    assert grouped == {
+        1.000000005: [1, 1.00000001],
+        2.0: [2],
+        3.00000001: [3, 3.00000002],
+    }
+
+    # Now with tuples and a key
+    data = [
+        ("e", 3.00000002),
+        ("a", 1),
+        ("b", 1.00000001),
+        ("c", 2),
+        ("d", 3),
+        ("f", 3),
+        ("g", 3.00000002),
+    ]
+    grouped = group_by_closeness(data, key=lambda x: x[1])
+
+    assert len(grouped) == 3
+    assert grouped == {
+        1.000000005: [("a", 1), ("b", 1.00000001)],
+        2.0: [("c", 2)],
+        3.00000001: [("d", 3), ("f", 3), ("e", 3.00000002), ("g", 3.00000002)],
+    }
+
+    # Test different tolerances
+    data = [1, 1.0001, 2, 3, 3.0001]
+
+    strict_groups = group_by_closeness(data, atol=0.00001)
+    lenient_groups = group_by_closeness(data, atol=0.001)
+
+    assert len(strict_groups) == 5
+    assert len(lenient_groups) == 3
+
+    assert np.allclose(list(strict_groups.keys()), data)
+    assert np.allclose(list(sorted(lenient_groups.keys())), [1.00005, 2.0, 3.00005])
