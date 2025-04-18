0e719405041c6893d5f77d2b78be1d0392f86e16
Simone Sturniolo <simonesturniolo@gmail.com>
2025-04-18 15:23:01 +0100

    Added example

--- examples/02_rational_function_calculus.py
+++ examples/02_rational_function_calculus.py
@@ -36,7 +36,14 @@ ratfunc_second_derivative = ratfunc.deriv(2)
 ratfunc_num_second_gradient = np.gradient(ratfunc_num_gradient, x)
 
 fig, ax = plt.subplots()
-ax.plot(x, ratfunc_second_derivative(x), label="RationalFunction.diff", c="b", lw=0.8, ls="-")
+ax.plot(
+    x,
+    ratfunc_second_derivative(x),
+    label="RationalFunction.diff",
+    c="b",
+    lw=0.8,
+    ls="-",
+)
 ax.plot(x, ratfunc_num_second_gradient, label="np.gradient", c="g", lw=0.8, ls="--")
 ax.legend()
 ax.set_title("Second derivative of the rational function")
@@ -44,3 +51,22 @@ ax.set_xlabel("x")
 ax.set_ylabel("y")
 
 # %%
+# Integral
+
+ratfunc_integral = ratfunc.integ()
+ratfunc_num_integral = np.cumsum(ratfunc(x)) * (x[1] - x[0])
+
+fig, ax = plt.subplots()
+ax.plot(
+    x,
+    ratfunc_integral(x) - ratfunc_integral(x)[0],
+    label="RationalFunction.integ",
+    c="b",
+    lw=0.8,
+    ls="-",
+)
+ax.plot(x, ratfunc_num_integral, label="np.cumsum", c="g", lw=0.8, ls="--")
+ax.legend()
+ax.set_title("Integral of the rational function")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
